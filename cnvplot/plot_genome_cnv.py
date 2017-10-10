ab0666760ec34ae3288271b49d03acd5b8b1ca56
Wen-Wei Liao <wen-wei.liao@wustl.edu>
2017-10-10 00:03:26 -0500

    Update plot_genome_cnv.py

--- cnvplot/plot_genome_cnv.py
+++ cnvplot/plot_genome_cnv.py
@@ -4,15 +4,18 @@ from os.path import basename, splitext
 
 import HTSeq
 import numpy as np
-import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-c", "--chromsize", help="Index file of reference genome")
-parser.add_argument("-x", "--wxs", help="Output from GATK4 CNV using whole exome sequencing (WXS) data")
-parser.add_argument("-g", "--wgs", help="Output from BIC-Seq2 using whole genome sequencing (WGS) data")
-parser.add_argument("-w", "--winsize", type=int, default=10000, help="Moving window size. (default: %(default)s)")
+parser.add_argument("-c", "--chromsize", metavar="FILE", help="Index file of "
+                    "reference genome")
+parser.add_argument("-x", "--wxs", metavar="FILE", help="Output from GATK4 "
+                    "CNV using whole exome sequencing (WXS) data")
+parser.add_argument("-g", "--wgs", metavar="FILE", help="Output from BIC-seq2 "
+                    "using whole genome sequencing (WGS) data")
+parser.add_argument("-w", "--winsize", metavar="INT", type=int, default=10000,
+                    help="Moving window size. (default: %(default)s)")
 args = parser.parse_args()
 
 included_chroms = list(map(str, range(1, 23))) + ["X", "Y"]
@@ -29,11 +32,11 @@ wxs_array = HTSeq.GenomicArray(chrom_lens, stranded=False, typecode="d")
 with open(args.wxs) as infile:
     for i, line in enumerate(infile):
         if i > 0:
-            sample, chrom, start, end, numprobes, segmean, segcall = line.strip().split("\t")
-            chrom = "chr" + chrom 
-            start = int(start) - 1
-            end = int(end)
-            cnv = float(segmean)
+            line = line.strip().split("\t")
+            chrom = "chr" + line[1]
+            start = int(line[2]) - 1
+            end = int(line[3])
+            cnv = float(line[5])
             iv = HTSeq.GenomicInterval(chrom, start, end, ".")
             wxs_array[iv] += cnv
 
@@ -56,52 +59,60 @@ ax1 = fig.add_subplot(211)
 ax2 = fig.add_subplot(212)
 prelen = 0
 x = []
-gy = []
-xy = []
+wgs_y = []
+wxs_y = []
 for chrom in included_chroms:
     chrom = "chr" + chrom
     for start in range(0, chrom_lens[chrom] - args.winsize, args.winsize):
         end = start + args.winsize
         iv = HTSeq.GenomicInterval(chrom, start, end)
-        x.append(0.5*(prelen + start + prelen + end))
-        gy.append(np.mean(list(wgs_array[iv])))
-        xy.append(np.mean(list(wxs_array[iv])))
+        x.append(prelen + 0.5*(start + end))
+        wxs_window = list(wxs_array[iv])
+        wxs_y.append(sum(wxs_window)/len(wxs_window))
+        wgs_window = list(wgs_array[iv])
+        wgs_y.append(sum(wgs_window)/len(wgs_window))
     prelen += chrom_lens[chrom]
 
-ax1.plot(x, xy, "o", ms=6, alpha=1)
+ax1.plot(x, wxs_y, "o", ms=6, alpha=0.6)
 prelen = 0
 xticks = []
-xlabels = []
+xticklabels = []
 for chrom in included_chroms:
-    chrom = "chr" + chrom
-    xlabels.append(chrom)
+    xticklabels.append(chrom)
     if prelen > 0:
-        ax1.axvline(prelen, color="grey", ls="--", alpha=0.6)
-    xticks.append(0.5*(prelen + prelen + chrom_lens[chrom]))
-    prelen += chrom_lens[chrom]
+        ax1.axvline(prelen, color="gray", ls="--", alpha=0.6)
+    length = chrom_lens["chr" + chrom]
+    xticks.append(prelen + 0.5*(length))
+    prelen += length
 ax1.set_xlim(0, prelen)
 ax1.set_ylim(0, 2)
 ax1.set_xticks(xticks)
-ax1.set_xticklabels(xlabels)
+ax1.set_xticklabels(xticklabels)
+ax1.tick_params(labelsize="xx-large")
+ax1.set_xlabel("Chromosome", size="xx-large")
 ax1.set_ylabel("WXS", size="xx-large")
 
-ax2.plot(x, gy, "o", ms=6, alpha=1)
+ax2.plot(x, wgs_y, "o", ms=6, alpha=0.6)
 prelen = 0
 xticks = []
-xlabels = []
+xticklabels = []
 for chrom in included_chroms:
-    chrom = "chr" + chrom
-    xlabels.append(chrom)
+    xticklabels.append(chrom)
     if prelen > 0:
-        ax2.axvline(prelen, color="grey", ls="--", alpha=0.6)
-    xticks.append(0.5*(prelen + prelen + chrom_lens[chrom]))
-    prelen += chrom_lens[chrom]
+        ax2.axvline(prelen, color="gray", ls="--", alpha=0.6)
+    length = chrom_lens["chr" + chrom]
+    xticks.append(prelen + 0.5*(length))
+    prelen += length
 ax2.set_xlim(0, prelen)
 ax2.set_ylim(0, 2)
 ax2.set_xticks(xticks)
-ax2.set_xticklabels(xlabels)
+ax2.set_xticklabels(xticklabels)
+ax2.tick_params(labelsize="xx-large")
+ax2.set_xlabel("Chromosome", size="xx-large")
 ax2.set_ylabel("WGS", size="xx-large")
 
 plt.tight_layout()
-plt.savefig("{0}_{1}.genomecnv.png".format(splitext(basename(args.wxs))[0], splitext(basename(args.wgs))[0]), dpi=300)
+prefix1 = splitext(basename(args.wxs))[0]
+prefix2 = splitext(basename(args.wgs))[0]
+plt.savefig("{0}_{1}.genomecnv.png".format(prefix1, prefix2, dpi=300))
 plt.close()
